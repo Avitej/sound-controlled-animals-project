80f8df0e98ae1c520c368bcaeb13fe6dd10c04a0
Avitej <82682098+Avitej@users.noreply.github.com>
2022-02-06 15:30:46 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,38 @@ function startClassification () {
     navigator.mediaDevices.getUserMedia({
         audio: true
     });
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/l29H77W-3/', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/l29H77W-3/model.json', modelReady);
 }
 function modelReady() {
     classifier.classify(gotResult);
 }
 
 function gotResult (error, results) {
-    console.log('got result')
-}
+    if (error) {
+        console.error(error);
+      } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+    
+        document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    
+        image = document.getElementById('image')
+    
+        if (results[0].label == "bark") {
+            image.src = 'dog.png';
+        } else if (results[0].label == "meow") {
+            image.src = 'cat.png';
+        } else if (results[0].label == "moo") {
+            image.src = 'cow.png';
+        }else if(results[0].label == "roar") {
+            image.src = 'lion.jpg';
+        }else{
+            image.src = 'Ear.png';
+        }
+      }
+    }
